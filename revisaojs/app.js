c389f1cffb11fb5349e813c8f8cd938fbf9e8190
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-21 09:26:55 -0300

    aula revisao js

---
+++ revisaojs/app.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')()
+
+/*function teste(){
+    var nome1 = 'Caio'
+    console.log(nome1)
+}
+teste() */
+
+/*if(true){
+    let nome1 = 'caio'
+
+}
+console.log(nome1)*/
+
+/*printar()
+var nome1
+function printar(){
+    console.log(nome1 = 'caio')
+}*/
+
+/* const nome1 = 'Caio'
+console.log(nome1.toUpperCase())
+
+console.log(`Ola ${nome1==='Caio'? 'Ola':'Nao te conheco'}`)
+
+console.log('KKLADJLAWJDALKSDJALWKDJALSDKJALWDJKA') */
+
+
